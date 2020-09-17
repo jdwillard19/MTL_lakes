4b151d71e0a27b14c97beb51eb2c4b683c009214
Jared W <willa099@umn.edu>
2020-09-16 19:33:43 -0500

    geometry JSON parse debug

--- src/data/preprocess_morphometry.py
+++ src/data/preprocess_morphometry.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import re
 import json
+import pdb
 ######################################################################################
 # (Jared - Sept 2020) parse morphometry from USGS data release into geometry files for PGDL
 ####################################################################################
