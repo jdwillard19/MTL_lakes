114e60b9b6201ea2c86ed396d9d39db8747191d3
Jared W <willa099@umn.edu>
2020-09-16 19:30:26 -0500

    geometry JSON parse debug

--- src/data/getMorphometryForNewLakesNov2019.py
+++ src/data/getMorphometryForNewLakesNov2019.py
@@ -5,7 +5,7 @@ import json
 ######################################################################################
 # (Jared - Sept 2020) parse morphometry from USGS data release into geometry files for PGDL
 ####################################################################################
-ids = pd.read_csv("../../metadata/lake_metadata.feather")
+ids = pd.read_feather("../../metadata/lake_metadata.feather")
 cfg_path = "../../data/raw/sb_mtl_data_release/pb0_config.json"
 ids = ids['site_id'].values
 
