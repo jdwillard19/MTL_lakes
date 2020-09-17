62c8012669749df2eb215d5aaa369efa880bb5cd
Jared W <willa099@umn.edu>
2020-09-16 20:43:24 -0500

    preprocess debug

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -16,8 +16,9 @@ import datetime
 
 #inital data load, site ids
 base_path = "../../data/raw/sb_mtl_data_release/"
+obs_df = pd.read_csv(base_path+"obs/temperature_observations.csv")
 metadata = pd.read_feather("../../metadata/lake_metadata.feather")
-ids = metadata['site_id'].values
+ids = np.unique(obs_df['site_id'].values)
 n_features = 7
 
 n_lakes = ids.shape[0]
