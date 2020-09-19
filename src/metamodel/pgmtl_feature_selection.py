6dd4e0efb1b5a40011336184f058ca54284c9dfd
Jared W <willa099@umn.edu>
2020-09-19 17:46:26 -0500

    debug metadata create

--- src/metamodel/pgmtl_feature_selection.py
+++ src/metamodel/pgmtl_feature_selection.py
@@ -37,6 +37,7 @@ for _, lake_id in enumerate(train_lakes):
 	lake_df = pd.read_feather("../../metadata/diffs/target_nhdhr_"+lake_id+".feather")
 	lake_df = lake_df[np.isin(lake_df['site_id'], train_lakes_wp)]
 	lake_df_res = lake_df_res[np.isin(lake_df_res['source_id'], train_lakes)]
+	pdb.set_trace()
 	lake_df = pd.merge(left=lake_df, right=lake_df_res.astype('object'), left_on='site_id', right_on='source_id')
 	new_df = lake_df
 	train_df = pd.concat([train_df, new_df], ignore_index=True)
