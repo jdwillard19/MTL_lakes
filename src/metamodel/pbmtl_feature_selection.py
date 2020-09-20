2e8ab977cecfa96b7170dfa3e38d134805ec5b3a
Jared W <willa099@umn.edu>
2020-09-20 13:41:51 -0500

    hyperparam search pbmtl

--- src/metamodel/pbmtl_feature_selection.py
+++ src/metamodel/pbmtl_feature_selection.py
@@ -8,6 +8,12 @@ from sklearn.feature_selection import RFECV
 import math
 import re 
 
+
+
+############################################################################
+# (Sept 2020 - Jared) PB-MTL feature selection (RFECV)
+################################################################################
+
 #load GLM transfer 
 glm_all_f = pd.read_csv("../../results/glm_transfer/RMSE_transfer_glm_pball.csv")
 train_df = pd.read_feather("../../results/glm_transfer/glm_meta_train_data.feather")
@@ -17,45 +23,25 @@ n_lakes = len(train_lakes)
 
 
 
-############################################################################
-#use CV to find which to drop
-################################################################################
-pdb.set_trace()
+#candidate features
 feats = train_df.columns[81:-1]
 
-
+#training data
 est = GradientBoostingRegressor(n_estimators=800,learning_rate=0.1)
 X = pd.DataFrame(train_df[feats])
 y = np.ravel(pd.DataFrame(train_df['rmse']))
-#ENABLE FOR SHORT DEBUG RUN
+
+#do recursive feature elimination cross validation (RFECV)
 rfecv = RFECV(estimator=est, cv=24, step=2, scoring='neg_mean_squared_error', verbose=1, n_jobs=24)
 rfecv.fit(X, y)
 
 print("Optimal number of features : %d" % rfecv.n_features_)
 
-# Plot number of features VS. cross-validation scores
+
 print("ranking: ", rfecv.ranking_)
 
 print("scores: ", rfecv.grid_scores_)
 
 
-
+#print final features
 print(feats[rfecv.ranking_ == 1])
-
-# writeF.close()
-# print("mean test RMSE: ",rmse_per_testlake.mean())
-# 
-
-
-# df['rmse_pred'] = y_pred
-# df = df.sort_values(by=['rmse_pred'])
-# print(df)
-#assess performance of the model
-
-
-# scores = []
-# kfold = KFold(n_splits=10, shuffle=True, random_state=42)
-# for i, (train, test) in enumerate(kfold.split(X, y)):
-# 	model.fit(X.iloc[train,:], y.iloc[train,:])
-# 	score = model.score(X.iloc[test,:], y.iloc[test,:])
-# 	scores.append(score)
