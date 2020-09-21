5e622c0d52682b5390b5c568e7ed226db6d40b7b
Jared W <willa099@umn.edu>
2020-09-20 21:35:31 -0500

    pgmtl train debug

--- src/metamodel/pgmtl_train_metamodel.py
+++ src/metamodel/pgmtl_train_metamodel.py
@@ -35,6 +35,7 @@ ids = pd.read_csv('../../metadata/pball_site_ids.csv', header=None)
 ids = ids[0].values
 glm_all_f = pd.read_csv("../../results/glm_transfer/RMSE_transfer_glm_pball.csv")
 train_lakes = [re.search('nhdhr_(.*)', x).group(1) for x in np.unique(glm_all_f['target_id'].values)]
+train_lakes_wp = np.unique(glm_all_f['target_id'].values) #with prefix
 
 
 
