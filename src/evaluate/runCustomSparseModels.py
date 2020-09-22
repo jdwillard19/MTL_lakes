02d4579ce6756ed6284edb755a6029f50a69c8c0
Jared W <willa099@umn.edu>
2020-09-21 19:50:06 -0500

    run custom sparse debug

--- src/evaluate/runCustomSparseModels.py
+++ src/evaluate/runCustomSparseModels.py
@@ -99,6 +99,8 @@ save = True
 for lake_ct, lakename in enumerate(test_lakes):
     if os.path.exists("../../results/"+lakename+"/sparseModelResults.csv"):
         continue
+    if lake_ct < 225:
+        continue
     print("(",lake_ct,"/",len(test_lakes),"): ", lakename)
     data_dir = "../../data/processed/"+lakename+"/"
 
